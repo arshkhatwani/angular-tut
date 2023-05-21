d18b2a8ba05482d40a42b4ab20a08955cb0eb7f0
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-05-21 15:04:38 +0530

    Saves todos in local storage when they are added/deleted

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,15 +10,17 @@ export class TodosComponent {
   todos: Todo[];
 
   constructor() {
-    this.todos = [];
+    this.todos = JSON.parse(localStorage.getItem('todos') || '[]');
   }
 
   deleteTodo(todo: Todo) {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
   addTodo(todo: Todo) {
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
